9294481a93758e5dd67235ac49bd759bf8b5485c
Frode Egeland <egeland@gmail.com>
2017-08-06 08:34:08 +1000

    tweak dns check

--- dnscheck.sh
+++ dnscheck.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 
-if [[ $(getent hosts ${VARNISH_BACKEND_ADDRESS} | awk '{print $1}') = $VARNISH_BACKEND_ADDRESS ]] 2>/dev/null ; then
+if [[ $(getent ahostsv4 ${VARNISH_BACKEND_ADDRESS} | awk '{print $1}' |head -n 1) = $VARNISH_BACKEND_ADDRESS ]] 2>/dev/null ; then
   echo "WARNING: Backend appears to be an IP address, no need to refresh dns"
   exit 0
 fi
 
-LOOKUP_ADDRESS="${VARNISH_BACKEND_ADDRESS}"
-
-if [[ ! -z $KUBERNETES_SERVICE_HOST ]] && ! getent hosts ${LOOKUP_ADDRESS} >/dev/null; then
-  LOOKUP_ADDRESS="${LOOKUP_ADDRESS}.svc.cluster.local"
-fi
-
-if ! getent hosts ${LOOKUP_ADDRESS} >/dev/null; then
-  echo "ERROR: ${LOOKUP_ADDRESS} is not a valid address"
+if ! getent hosts ${VARNISH_BACKEND_ADDRESS} >/dev/null; then
+  echo "ERROR: ${VARNISH_BACKEND_ADDRESS} is not a valid address"
   exit 1
 fi
 
@@ -23,7 +17,7 @@ while true; do
   sleep ${VARNISH_DNS_TTL:-17}
 
   # Check DNS
-  dig "${LOOKUP_ADDRESS}" +short | head -n 1 > /tmp/lookup.new
+  getent ahostsv4 "${VARNISH_BACKEND_ADDRESS}" |awk '{print $1}' | head -n 1 > /tmp/lookup.new
 
   # Compare old vs new
   cmp -s /tmp/lookup.new /tmp/lookup.curr
