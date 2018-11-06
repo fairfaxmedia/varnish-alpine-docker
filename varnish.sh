f4afff7790a331db164e554d4ade5a0ae2a12455
Alexey Shabalin <alexey.shabalin@fairfaxmedia.com.au>
2018-11-06 13:41:53 +1100

    Moved logging into a separate service.

--- varnish.sh
+++ varnish.sh
@@ -3,12 +3,11 @@ SLEEP_TIME=15
 
 start_varnish()
 {
-  timestp="stamp_$(date +%F_%H:%M:%S)"
   if [ -f "/tmp/backend-only" ]; then
-    echo "varnish.sh: [${timestp}] Using only backend ${VARNISH_BACKEND_ADDRESS}:${VARNISH_BACKEND_PORT}, no config file"
+    echo "varnish.sh: Using only backend ${VARNISH_BACKEND_ADDRESS}:${VARNISH_BACKEND_PORT}, no config file"
     varnishd -s malloc,${VARNISH_MEMORY} -a :80 -b ${VARNISH_BACKEND_ADDRESS}:${VARNISH_BACKEND_PORT}
   else
-    echo "varnish.sh: [${timestp}] Using supplied ${VARNISH_CONFIG_FILE}"
+    echo "varnish.sh: Using supplied ${VARNISH_CONFIG_FILE}"
     varnishd -f ${VARNISH_CONFIG_FILE} -s malloc,${VARNISH_MEMORY}
   fi
 }
@@ -21,23 +20,9 @@ wait_for_varnish()
   done
 }
 
-enable_logging()
-{
-  if [[ -z $DEBUG ]]; then
-    : ${VARNISH_NCSA_FORMAT:='%h %l %u %t "%r" %s %b "%{Referer}i" "%{User-agent}i"'}
-    CHECK_STR="varnishncsa -F"
-    [[ $(ps | grep "${CHECK_STR}" | grep -v "grep" | wc -l) = 0 ]] && varnishncsa -F "${VARNISH_NCSA_FORMAT}"
-  else
-    echo "Debug logging enabled"
-    CHECK_STR="varnishlog"
-    [ $(ps | grep "${CHECK_STR}" | grep -v "grep" | wc -l) = 0 ]] && varnishlog
-  fi
-}
-
 while true; do
   VARNISH_STR="varnishd -f"
   [[ $(ps | grep "${VARNISH_STR}" | grep -v "grep" | wc -l) = 0 ]] && start_varnish
   wait_for_varnish
-  enable_logging
   sleep ${SLEEP_TIME}
 done
