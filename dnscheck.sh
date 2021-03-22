d3409dd1d75261368bd73ef906e065f1c27b7a2b
matt hope <matt.hope@fairfaxmedia.com.au>
2021-03-23 09:56:23 +1100

    fix(dnscheck): The backend list can contain a port, which breaks DNS lookups.

--- dnscheck.sh
+++ dnscheck.sh
@@ -7,7 +7,7 @@ done
 
 get_backends()
 {
-  BACKEND_LIST=$(varnishadm vcl.show $(varnishadm vcl.list | grep active | awk '{print $4}') | egrep '^[ \t]+\.host[ \t]*=' | grep -v nodnscheck | cut -d '"' -f 2 | sort | uniq )
+  BACKEND_LIST=$(varnishadm vcl.show $(varnishadm vcl.list | grep active | awk '{print $4}') | egrep '^[ \t]+\.host[ \t]*=' | grep -v nodnscheck | cut -d '"' -f 2 | cut -d: -f1 | sort | uniq )
   > /tmp/backend.list
   for backend in ${BACKEND_LIST}; do
 
